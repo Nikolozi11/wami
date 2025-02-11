49c3252959a86d4cb524d95702758a218c21ff7e
SiteStructor <sitestructor24@gmail.com>
2025-02-11 21:49:02 +0400

    second commit

--- function.js
+++ function.js
@@ -4,3 +4,5 @@ const navLinks = document.getElementById('navLinks');
 burger.addEventListener('click', () => {
     navLinks.classList.toggle('active'); // ღილაკზე დაჭერისას მენიუ გამოჩნდება ან დაიმალება
 });
+
+
